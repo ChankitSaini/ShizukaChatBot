6f626270ac42525c6c2fc36c3d6d60c6b6e5a093
NeuroticCoders <87204519+NeuroticCoders@users.noreply.github.com>
2021-07-24 11:47:48 +0530

    NameError: name 'Shizuka' is not defined

--- shizuka/ShizukaChatBot/start.py
+++ shizuka/ShizukaChatBot/start.py
@@ -7,8 +7,8 @@ I am Shizuka 『しずか』, An Intelligent ChatBot.[⠀](https://telegra.ph/fi
 """
 
 
-@Shizuka.on_message(filters.command(["start"], prefixes = ["/" , "!"]) & ~filters.edited)
+@SHIZUKA.on_message(filters.command(["start"], prefixes = ["/" , "!"]) & ~filters.edited)
 async def info(client, message):
     buttons = [[InlineKeyboardButton(text="Go inline", switch_inline_query_current_chat="shizuka "),],
                    [InlineKeyboardButton("Github", url="https://github.com/NeuroticCoders/ShizukaChatBot"), InlineKeyboardButton("Maintained by", url = "https://t.me/NeuroticAssociation")]]
-    await Shizuka.send_message(chat_id = message.chat.id, text = SHIZUKA_START, reply_markup = InlineKeyboardMarkup(buttons))
+    await SHIZUKA.send_message(chat_id = message.chat.id, text = SHIZUKA_START, reply_markup = InlineKeyboardMarkup(buttons))
