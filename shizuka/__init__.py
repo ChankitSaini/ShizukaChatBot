b7d91f7a7d7de4f86d9865c029fa7943cab498aa
NeuroticCoders <87204519+NeuroticCoders@users.noreply.github.com>
2021-07-24 11:52:01 +0530

    Update __init__.py

--- shizuka/__init__.py
+++ shizuka/__init__.py
@@ -5,4 +5,4 @@ API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 TOKEN = os.environ.get("TOKEN", None)
 
-Shizuka = Client(':memory:', api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+SHIZUKA = Client(':memory:', api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
