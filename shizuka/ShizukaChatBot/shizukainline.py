03700166fe2a2a07c71b07d4f8f8e38a52cc4683
NeuroticCoders <87204519+NeuroticCoders@users.noreply.github.com>
2021-07-24 11:29:42 +0530

    Inline

---
+++ shizuka/ShizukaChatBot/shizukainline.py
@@ -0,0 +1,76 @@
+import os
+from urllib.parse import unquote, urlparse
+import re
+import traceback
+import sys
+import random
+import aiohttp
+import requests
+import traceback
+from shizuka import ShizukaChatBot
+from datetime import datetime
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    InputTextMessageContent,
+    InlineQueryResultArticle,
+    InlineQueryResultPhoto,
+    CallbackQuery,
+    InlineQuery,
+    InlineQueryResultAnimation,
+)   
+from pykeyboard import InlineKeyboard
+
+async def fetch(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            try:
+                data = await resp.json()
+            except Exception:
+                data = await resp.text()
+    return data
+
+
+@Shizuka.on_inline_query()
+async def inline_query_handler(client, query):
+    string = query.query.lower()
+
+    answers = []
+    if string.split()[0] == "shizuka":
+            if len(string.split()) < 2:
+                await client.answer_inline_query(
+                    query.id,
+                    results=answers,
+                    switch_pm_text='Shizuka | Chat [text]',
+                    switch_pm_parameter='shizuka',
+                )
+                return
+            shizuka = string.split(None, 1)[1].strip()
+            Shizuka = await shizukachatbot(answers, shizuka)
+            await client.answer_inline_query(
+                query.id,
+                results=Shizuka,
+                cache_time=2
+            )
+   
+
+async def shizukachatbot(answers, text):
+    URL = f"https://api.affiliateplus.xyz/api/chatbot?message={text}&botname=@ShizukaChatBot&ownername=@ChankitSaini"
+    result = await fetch(URL)
+    buttons = InlineKeyboard(row_width=1)
+    buttons.add(InlineKeyboardButton(
+        "Shizuka",
+        switch_inline_query_current_chat="shizuka"
+    ))
+    caption = f"""
+**You:** `{text}`
+**Shizuka:** `{result['message']}`"""
+    answers.append(
+        InlineQueryResultPhoto(
+            photo_url="https://telegra.ph/file/c8f2e290ba36052058154.jpg",
+            title="Shizuka",
+            description=result['message'],
+            caption=caption,
+            reply_markup=buttons
+        ))
+    return answers
