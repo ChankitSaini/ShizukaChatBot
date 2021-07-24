76527a19df492fb0992edaff6f639cb8b6525dac
NeuroticCoders <87204519+NeuroticCoders@users.noreply.github.com>
2021-07-24 11:15:00 +0530

    Initial Commit

---
+++ shizuka/__init__.py
@@ -0,0 +1,8 @@
+import os
+from pyrogram import Client
+
+API_ID = os.environ.get("API_ID", None)
+API_HASH = os.environ.get("API_HASH", None)
+TOKEN = os.environ.get("TOKEN", None)
+
+Shizuka = Client(':memory:', api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
